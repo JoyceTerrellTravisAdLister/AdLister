e52bdba729c7187330366cbe293b5c872b90fd4b
Elvin <ttop34@icloud.com>
2017-10-26 09:50:19 -0500

    create Ad model

---
+++ src/main/java/models/Ad.java
@@ -0,0 +1,47 @@
+package models;
+
+public class Ad {
+    private long userId;
+    private long id;
+    private String title;
+    private String description;
+
+    public Ad(long id, long userId, String title, String description){
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
