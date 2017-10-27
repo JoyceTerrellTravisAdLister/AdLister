d104a90f86042a31f292112e79451529043cae31
Travis Payne <thpaggie910@gmail.com>
2017-10-27 16:00:04 -0500

    Update users table to have a created_at column

---
+++ src/main/migrations.sql
@@ -0,0 +1,23 @@
+USE jtt_adlister_db;
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id       BIGINT UNSIGNED     NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) UNIQUE  NOT NULL,
+  email    VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(100)        NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+  id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id     BIGINT UNSIGNED NOT NULL,
+  title       VARCHAR(255)    NOT NULL,
+  description TEXT            NOT NULL,
+  created_at  DATETIME        NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
+);
