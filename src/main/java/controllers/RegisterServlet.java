ffd782e62cfe14958daa75cd0bbf81a538b97bf0
ironicsushi <yuli5565@colorado.edu>
2017-10-27 13:21:43 -0500

    * complete error messages for all fields in register page

--- src/main/java/controllers/RegisterServlet.java
+++ src/main/java/controllers/RegisterServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.HashMap;
 
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
@@ -26,10 +26,52 @@ public class RegisterServlet extends HttpServlet {
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        String confirm = request.getParameter("confirm-password");
+
+
+        if (DaoFactory.getUsersDao().getUserByUsername(username) != null) {
+            request.setAttribute("error", "This username is already taken.");
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            return;
+        }
+
+        HashMap<String, String> errors = new HashMap<>();
+
+        if (username.isEmpty()) {
+            errors.put("username", "Username cannot be left blank");
+        } else {
+            request.setAttribute("username", username);
+        }
+
+        if (email.isEmpty()) {
+            errors.put("email", "Email cannot be left blank");
+        } else {
+            request.setAttribute("email", email);
+        }
+
+        if (password.isEmpty()) {
+            errors.put("password", "Password cannot be left blank");
+        } else {
+            request.setAttribute("password", password);
+        }
+
+        if (!password.equals(confirm)) {
+            errors.put("confirm", "Passwords do not match");
+        } else {
+            request.setAttribute("confirm", confirm);
+        }
+
+        request.setAttribute("errors", errors);
+
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty() || !password.equals(confirm)) {
+            request.getRequestDispatcher("WEB-INF/register.jsp").forward(request, response);
+            return;
+        }
 
         User user = new User(username, email, password);
         DaoFactory.getUsersDao().insertUser(user);
-        response.sendRedirect("/login");
-    }
 
+        response.sendRedirect("/login");
+
+    }
 }
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <jsp:include page="partials/head.jsp">
@@ -13,21 +14,40 @@
         <div class="form-group">
            <label for="username">Username</label>
             <input id="username" name="username" class="form-control" type="text">
+            <c:if test="${error != null}">
+                <span>${error}</span>
+            </c:if>
+
+            <c:if test="${errors.containsKey('username')}">
+                <span class="errors">${errors.get('username')}</span>
+            </c:if>
+
+
+
         </div>
         <%--Email--%>
         <div class="form-group">
             <label for="email">Email</label>
             <input id="email" name="email" class="form-control" type="text">
+            <c:if test="${errors.containsKey('email')}">
+                <span class="errors">${errors.get('email')}</span>
+            </c:if>
         </div>
         <%--Password--%>
         <div class="form-group">
             <label for="password">Password</label>
             <input id="password" name="password" class="form-control" type="password">
+            <c:if test="${errors.containsKey('password')}">
+                <span class="errors">${errors.get('password')}</span>
+            </c:if>
         </div>
         <%--Confirm password--%>
         <div class="form-group">
             <label for="confirm-password">Confirm Password</label>
             <input id="confirm-password" name="confirm-password" class="form-control" type="password">
+            <c:if test="${errors.containsKey('confirm')}">
+                <span class="errors">${errors.get('confirm')}</span>
+            </c:if>
         </div>
         <input type="submit" class="btn btn-primary btn-block">
     </form>
