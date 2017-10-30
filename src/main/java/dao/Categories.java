57bf1f4821ae26a660cf54c8f351efa989e7de24
Travis Payne <thpaggie910@gmail.com>
2017-10-30 15:38:50 -0500

    Create a `MySQLCategoriesDao` Data Access Object

--- src/main/java/dao/Categories.java
+++ src/main/java/dao/Categories.java
@@ -8,4 +8,6 @@ public interface Categories {
     List<Category> getAll();
     List<Category> getCategoriesByAdId(long id);
     Category getCategoryById(long id);
+    long insert(Category categoryToInsert);
+    void assignCategoryToAd(long category_id, long ad_id);
 }
